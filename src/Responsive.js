fda1e64ce06e207f8de53dfa8b8359ae3ca6b151
“Ravi <patilravi400@gmail.com>
2022-12-11 23:43:44 +0530

    MERN App

---
+++ src/Responsive.js
@@ -0,0 +1,18 @@
+import { css } from "styled-components"
+
+export const mobile = (props) =>{
+    return css`
+    @media only screen and (max-width:425px){
+    ${props}
+  }
+    
+`;
+}
+// export const Tablet = (props) =>{
+//     return css`
+//     @media only screen and (max-width:425px){
+//     ${props}
+//   }
+    
+// `;
+// }
---
+++ src/requestMethods.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8000/api/";
+//const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOTIxYjFjYjQwNTBjOWI1MmIzMTUxMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3MDY5MTMwMywiZXhwIjoxNjcwOTUwNTAzfQ.-1TN9o71FTMbna8uPBWO5rAsWlRdVgpwiW1ZXFWDOi4"
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
+
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  header: { token: `Bearer ${TOKEN}` },
+});
